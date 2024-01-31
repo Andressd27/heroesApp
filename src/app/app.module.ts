5ddb5407f3480490267cd698c56910b5b2a46604
Andres Duran <andurand27@gmail.com>
2024-01-30 21:03:22 -0500

    estilo para el login con angular material y primeflex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
@@ -11,8 +12,9 @@ import { ShareModule } from './share/share.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    ShareModule
+    ShareModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
